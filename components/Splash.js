b516a233d6630e55b131fc459750f7cb81e27694
David Adokuru <davidadokuru@gmail.com>
2021-01-21 09:44:32 +0100

    first commit

---
+++ components/Splash.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import React,{Component} from 'react';
+import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
+
+const bgImage = require('../assets/splashBg.png');
+const imgLogo = require('../assets/logo.png');
+
+export default class Splash extends Component {
+  render(){
+    return (
+      <ImageBackground source={bgImage} style={styles.container}>
+        <View style={styles.logoBox}>
+          <Image source={imgLogo} style={{width: 300, height: 150, resizeMode: 'stretch',}}/>
+        </View>
+        <StatusBar style="auto" />
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoBox: {
+    backgroundColor: '#fff',
+    opacity: .9,
+    width: '100%',
+    alignItems: 'center',
+  }
+});
+
