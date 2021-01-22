f980b67d41c0f4c4debf3804f000e752e93b8953
adokuru <davidadokuru@gmail.com>
2021-01-22 11:55:37 +0100

    updates to splash screen

--- App.js
+++ App.js
@@ -1,5 +1,25 @@
-import { AppRegistry } from "react-native";
-import Splash from "./components/Splash";
-import {name as appName} from "./app.json";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-AppRegistry.registerComponent(appName, ()=> Splash);
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.texts}>New App!</Text>
+      <StatusBar style='auto' />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    color: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  texts: {
+    color: "black",
+  },
+});
