ae0104a5346ec45ee46ec69aedea3c0dc48bbfef
adokuru <davidadokuru@gmail.com>
2021-01-24 14:16:30 +0100

    updates

--- App.js
+++ App.js
@@ -1,76 +1,21 @@
-import React from "react";
-import { Platform, StatusBar, Image } from "react-native";
-import { AppLoading } from "expo";
-import { Asset } from "expo-asset";
-import { Block, GalioProvider } from "galio-framework";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { Images, products, materialTheme } from "./constants/";
-
-import { NavigationContainer } from "@react-navigation/native";
-import Screens from "./navigation/Screens";
-
-// Before rendering any navigation stack
-import { enableScreens } from "react-native-screens";
-enableScreens();
-
-// cache app images
-const assetImages = [
-  Images.Pro,
-  Images.Profile,
-  Images.Avatar,
-  Images.Onboarding,
-];
-
-// cache product images
-products.map((product) => assetImages.push(product.image));
-
-function cacheImages(images) {
-  return images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.fromModule(image).downloadAsync();
-    }
-  });
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <NavigationContainer>
-          <GalioProvider theme={materialTheme}>
-            <Block flex>
-              {Platform.OS === "ios" && <StatusBar barStyle='default' />}
-              <Screens />
-            </Block>
-          </GalioProvider>
-        </NavigationContainer>
-      );
-    }
-  }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([...cacheImages(assetImages)]);
-  };
-
-  _handleLoadingError = (error) => {
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
